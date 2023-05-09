617288fe58da62d567bffebd3f63258199a419ad
Caleb Oki <caleb.oki@ibm.com>
2023-05-08 21:42:07 -0400

    implemented authentication with sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PricingTemplateController;
+use App\Http\Controllers\Api\AbTestController;
+use App\Http\Controllers\Api\AnalyticsDataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Public routes for registration and login
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+// Protected routes using Sanctum middleware
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Logout route
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    // Pricing template management routes
+    Route::resource('pricing-templates', PricingTemplateController::class)->except(['create', 'edit']);
+
+    // A/B test management routes
+    Route::resource('ab-tests', AbTestController::class)->except(['create', 'edit']);
+
+    // Analytics data management routes
+    Route::resource('analytics-data', AnalyticsDataController::class)->except(['create', 'edit']);
 });
